e67105e0862accbea073fea900e0054e7135f318
Hu Kaixiang <maxhu787@gmail.com>
2022-11-01 12:51:08 +0800

    update

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ window.addEventListener('load', (event) => {
 });
 const input = document.getElementsByClassName("search-bar-main")[0];
 const searchBarCloseBtn = document.getElementsByClassName("search-bar-close-btn")[0];
-input.addEventListener('input', checkInput);
-function checkInput(e) {
+input.addEventListener('input', inputCheck);
+function inputCheck(e) {
     if (input.value.length >= 1) {
         searchBarCloseBtn.style.visibility = 'visible';
     }
@@ -14,15 +14,33 @@ function checkInput(e) {
 }
 function clearSearch() {
     input.value = "";
-    checkInput();
+    inputCheck();
 }
-function submit() {}
-
-$(".search-bar-main").on('keyup', function (e) {
+function submit(userInput) {
+    //https://www.google.com/search?q=test
+    //window.open('http://www.google.com'); 
+    let url = "https://www.google.com/search?q=".concat(userInput)
+    window.location.href = url;
+}
+function submissionCheck(e) {
     if (e.key === 'Enter' || e.keyCode === 13) {
-        console.log(input.value.trim());
-        if(input.value.trim().length !== 0) {
-            submit();
+        if (input.value.trim().length !== 0) {
+            let userInput = input.value.trim();
+            submit(userInput);
         }
     }
-});
+}
+$(".search-bar-main").on('keyup', submissionCheck);
+
+/*const headers = new Headers();
+headers.append('Cache-Control', 'no-cache');
+fetch('https://www.google.com/search?q=test', {
+    mode: 'no-cors'
+})
+    .then((response) => {
+        if (response.status != 200) {
+            console.log(response)
+        }
+    }, (err) => {
+        console.log('error: ' + err);
+    }, 5000);*/
